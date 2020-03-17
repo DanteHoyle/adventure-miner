9cb33db9554bbcab2960c73f810c99350fb8e3d6
dante <hoyledante@gmail.com>
2020-03-16 20:54:07 -0700

    sloppy but it works holy crap

--- js/miners.js
+++ js/miners.js
@@ -3,8 +3,29 @@ const FRAMETIME = 16.7; // 1/60th of second in miliseconds
 // This stores the state of the game
 game_state = {
     gold: 0,
+    goldPerSecond: 0,
+    goldPerClick: 1,
+}
+
+man = {
+    owned: 0,
     goldPerSecond: .5,
-    goldPerClick: 1
+    cost: 25,
+    upgradeMultipliar: 1.2
+}
+
+dude = {
+    owned: 0,
+    goldPerSecond: 5,
+    cost: 200,
+    upgradeMultipliar: 1.2
+}
+
+gentleman = {
+    owned: 0,
+    goldPerSecond: 25,
+    cost: 500,
+    upgradeMultipliar: 1.2
 }
 
 function updateControlPanel() {
@@ -17,13 +38,58 @@ function updateControlPanel() {
 }
 
 function update() {
-    game_state.gold += game_state.goldPerSecond * FRAMETIME * .001/*Constant to make it decimal*/;
-    
+    game_state.goldPerSecond = (man.owned * man.goldPerSecond);
+    game_state.goldPerSecond += (dude.owned * dude.goldPerSecond);
+    game_state.goldPerSecond += (gentleman.owned * gentleman.goldPerSecond)
+
+    game_state.gold += game_state.goldPerSecond * FRAMETIME * .001;
     updateControlPanel();
 }
 
 function userClickMine() {
     game_state.gold += game_state.goldPerClick;
+}
+
+
+// Awful terrible way to do this, but it works for now
+function purchase_man() {
+    // If user can afford
+    if (man.cost <= game_state.gold) {
+        man.owned += 1 // Adds miner
+        game_state.gold -= man.cost // removes gold
+        man.cost = Math.floor(man.cost * man.upgradeMultipliar);
+
+        cost_text = document.getElementById("man_cost");
+        owned_text = document.getElementById("man_owned");
+        cost_text.innerHTML = man.cost + "g";
+        owned_text.innerHTML = man.owned;
+    }
+}
+
+function purchase_dude() {
+    if (dude.cost <= game_state.gold) {
+        dude.owned += 1 // Adds miner
+        game_state.gold -= dude.cost // removes gold
+        dude.cost = Math.floor(dude.cost * dude.upgradeMultipliar);
+
+        cost_text = document.getElementById("dude_cost");
+        owned_text = document.getElementById("dude_owned");
+        cost_text.innerHTML = dude.cost + "g";
+        owned_text.innerHTML = dude.owned;
+    }
+}
+
+function purchase_gentleman() {
+    if (gentleman.cost <= game_state.gold) {
+        gentleman.owned += 1 // Adds miner
+        game_state.gold -= gentleman.cost // removes gold
+        gentleman.cost = Math.floor(gentleman.cost * gentleman.upgradeMultipliar);
+
+        cost_text = document.getElementById("gentleman_cost");
+        owned_text = document.getElementById("gentleman_owned");
+        cost_text.innerHTML = gentleman.cost + "g";
+        owned_text.innerHTML = gentleman.owned;
+    }
 }
 
 // Makes the loop happen
